91e40d542b5f4721994447b70d3b5bead835563d
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:22:34 +0200

    Update dowhileloop2.java

--- dowhileloop2.java
+++ dowhileloop2.java
@@ -1,11 +1,11 @@
-public class dowhileloop2 {
-public static void main(String args[]){
-    int b=1;
+public class dowhileloop2 {//A class called dowhileloop2.
+public static void main(String args[]){ java mandatory syntax.
+    int b=1;//assinment of initial value.
     do{
-        System.out.print("ok");
+        System.out.print("ok");//An output to be printed.
         b++;
     }
-        while(b<=20);
+        while(b<=20);// determination of maximum values.
     
 }
     }
