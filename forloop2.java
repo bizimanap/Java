fc91e7ef499425508456dbb56d1753ee5b4358ca
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:29:48 +0200

    Update forloop2.java

--- forloop2.java
+++ forloop2.java
@@ -1,11 +1,11 @@
-public class forloop2 {
-    public static void main (String args[]){
-        int a=1; 
-        for(a=1; a<=100; a++){
+public class forloop2 {// A class called forloop2.
+    public static void main (String args[]){ this is a a mandatory java syntax.
+        int a=1; //initiating of values.
+        for(a=1; a<=100; a++){//condition of formula.
             System.out.print("a");
 
         }
-        System.err.println();
+        System.err.println();//this line displays an output.
     }
 
     
