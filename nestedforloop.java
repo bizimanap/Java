9c4f631b8041bbe1d2789d2890eb7051fb92a838
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:34:39 +0200

    Update nestedforloop.java

--- nestedforloop.java
+++ nestedforloop.java
@@ -1,10 +1,10 @@
-class nestedforloop{
-    static public void main(String args[]){
-        int i;
-        int j;
-    int rows = 12;
+class nestedforloop{//this is a class called nestedloop.
+    static public void main(String args[]){//this is a mandatory java syntax.
+        int i;//first argument.
+        int j;//second argument.
+    int rows = 12;//assigniment of values.
 
-for (i= rows; i<= 12; i--) { 
+for (i= rows; i<= 12; i--) { //condition for loop.
   
             // loop to print the number of stars in each row 
             for (j = 1; j <= 12 ; j++) { 
