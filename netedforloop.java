0f77b983f899878e0985fca650fa3edd9350634c
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:39:50 +0200

    Update netedforloop.java

--- netedforloop.java
+++ netedforloop.java
@@ -1,10 +1,10 @@
-public class netedforloop {
-    public static void main(String arg[]){
-int b;
-int c;
-for(b=3; b<=8; b++){
-    for(c=3; c<=8; c--);
-  System.out.print("d");  
+public class netedforloop {//This is a first class called nestedloop.
+    public static void main(String arg[]){// a mandatory java syntax.
+int b;// first argument.
+int c;//second argument.
+for(b=3; b<=8; b++){//this is a condition for loop.
+    for(c=3; c<=8; c--);//this is a second condition for loop.
+  System.out.print("d"); //||this line displays an output to be printed by system. 
 }
     }
     
