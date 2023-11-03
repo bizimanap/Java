5983c71060f54c377dba0dab48f1858bacbd4de4
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 10:22:58 +0200

    Update dowhileloop1.java

--- dowhileloop1.java
+++ dowhileloop1.java
@@ -1,12 +1,12 @@
-public class dowhileloop1 {
-    public static void main(String args[]){
-        int a=1;
+public class dowhileloop1 {// A public class called dowhileloop.
+    public static void main(String args[]){// mandatory java syntax.
+        int a=1;// declaration of initial values.
         do{
-            System.out.print("hello");
+            System.out.print("hello");// an output to be printed.
             a++;
             
         }
-            while(a<=10);
+            while(a<=10);at //atleast minimum values.
         
     }
 }
