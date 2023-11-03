1144686d1f8580c8ccb23c331601e057e8b4f1fd
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:12:15 +0200

    Update breakif1.java

--- breakif1.java
+++ breakif1.java
@@ -1,10 +1,10 @@
-public class breakif1 {
-    public static void main(String args[]){
-        int i=1;
-        for(i=1; i<=5; i++){
-            if (i==4){
+public class breakif1 {//a class called breakif1.
+    public static void main(String args[]){// this is mandatory java syntax.
+        int i=1;// this is an initial value.
+        for(i=1; i<=5; i++){// an increamentation of values.
+            if (i==4){// assigniment.
                 break;
-            }System.out.print("$");
+            }System.out.print("$");// an output to   be printed.
         }
     }
     
