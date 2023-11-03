c8fc3c9de8df70b675a66fde52e1a72d04f4cc2e
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:45:08 +0200

    Update whileloop1.java

--- whileloop1.java
+++ whileloop1.java
@@ -1,8 +1,8 @@
-public class whileloop1 {
-    public static void main(String args[]){
-        int b=1;
-        while(b<=6){
-            System.out.print(b);
+public class whileloop1 {//This is a class called whileloop1.
+    public static void main(String args[]){//a mandtory java syntax.
+        int b=1;//assignment of value.
+        while(b<=6){//condition of formula.
+            System.out.print(b);//this line displays an output to be printed.
             b++;
         }
     }
