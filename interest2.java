58ff9b9532f5ab81ee86621e4c0f1b9fe693fa8e
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 11:07:42 +0200

    Update interest2.java

--- interest2.java
+++ interest2.java
@@ -1,5 +1,5 @@
  class interest2 {//a class called interest2.
-    public double cal(int p,int t,int r, int n){// created methood.
+    public double cal(double p,double t,duble r, double n){// created methood.
         returnp*(Math.pow((1+r/n),(n*t)));// the formula that will be used  to calculate.
 
     }
@@ -8,7 +8,7 @@
 class BB{// a second class called BB.
     public static void main(String[] args){// a mandatory java syntax.
         interest2 myinterest2=new interest2();// a created object called my interest2.
-        double response=myinterest2.cal(500000,18,3,12);// recalling of methood and the values .
+        double response=myinterest2.cal(500000,3,0.18,12);// recalling of methood and the values .
         System.out.println("response:"+response);//system prints an output.
     }
 
