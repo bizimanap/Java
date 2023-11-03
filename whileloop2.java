8d35f1c3798437fb5054a8823a4c226ada332c31
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:48:45 +0200

    Update whileloop2.java

--- whileloop2.java
+++ whileloop2.java
@@ -1,8 +1,8 @@
-public class whileloop2 {
-    public static void main(String args[]){
-        int f=1;
-        while(f<=100){
-            System.out.println("f");
+public class whileloop2 {//this is a class called whileloop2.
+    public static void main(String args[]){//This is amandatory java syntax.
+        int f=1;//assgning of value.
+        while(f<=100){//condition  of formula.
+            System.out.println("f");//an output to be printed.
             f--;
 
 
