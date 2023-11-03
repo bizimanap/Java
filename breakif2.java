dfacb6e67d0bea6c6249e12145d6b77e35291e42
bizimanap <149526716+bizimanap@users.noreply.github.com>
2023-11-03 19:18:36 +0200

    Update breakif2.java

--- breakif2.java
+++ breakif2.java
@@ -1,10 +1,10 @@
-public class breakif2 {
-    public static void main(String args[]){
-        int j;
-        for(j=1; j<=7; j++){
-            if(j==4){
+public class breakif2 {// A created class called breakif2.
+    public static void main(String args[]){//mandatory java syntax.
+        int j;//declaretion of argument.
+        for(j=1; j<=7; j++){//condition for decreament.
+            if(j==4){//Assigniment of values.
                 break;}
-                System.out.println(j);
+                System.out.println(j);//An output to be printed.
             }
         }
     }
